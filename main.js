ef0cfa64980831509c4a9c45c47e16081b0ecc5d
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2020-11-15 20:30:42 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let loc = document.getElementById('location-timezone');
 let temp = document.getElementById('temperature-degree');
 let desc = document.getElementById('temperature-description');
+let tempIcon = document.getElementById('icon');
 window.addEventListener('load', ()=> {
     let long;
     let lat;
@@ -16,15 +17,32 @@ window.addEventListener('load', ()=> {
                     return response.json();
                 })
                     .then(data => {
-                        const {name, country} = data;
+                        const {name} = data;
                         const {feels_like} = data.main;
                         const {id, main} = data.weather[0];
                         loc.textContent = name;
                         temp.textContent = feels_like;
                         desc.textContent = main;
-
+                        if(id < 250) {
+                            tempIcon.src = 'thunderstorms.svg';
+                        } else if (id < 350) {
+                            tempIcon.src = 'drizzle.svg';
+                        } else if (id < 600) {
+                            tempIcon.src = 'rain.svg';
+                        } else if (id < 700) {
+                            tempIcon.src = 'snow.svg';
+                        } else if(id > 700 && id < 800) {
+                            tempIcon.src = 'snow.svg';
+                        } else if(id === 800) {
+                            tempIcon.src = 'clear-day.svg';
+                        } else if(id > 800 && id < 803) {
+                            tempIcon.src = 'partly-cloudy-day.svg';
+                        } else {
+                            tempIcon.src = 'cloudy.svg';
+                        }
                         console.log(data);  
-                    })
+                    });
         });
     }
+  
 });
